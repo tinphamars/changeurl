b39f55db9f26042cc63d188062ea4116ed73da33
Tin <tyndecor@gmail.com>
2020-08-21 23:57:03 +0700

    fasdfa

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/build'))
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+    response.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 
 app.listen(port)
