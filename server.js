0100435d1f2d06aa93fc323847f64ea4f9fe12f8
Tin <tyndecor@gmail.com>
2020-08-21 23:49:01 +0700

    fix bug

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// const express = require('express')
-// const path = require('path')
-// const port = process.env.PORT || 5000
-// const app = express()
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 5000
+const app = express()
 
-// // serve static assets normally
-// app.use(express.static(__dirname + 'build'))
+// serve static assets normally
+app.use(express.static(__dirname + '/build'))
 
-// // handle every other route with index.html, which will contain
-// // a script tag to your application's JavaScript file(s).
-// app.get('*', function (request, response) {
-//     response.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-// })
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
-// app.listen(port)
-// console.log("server started on port " + port)
+app.listen(port)
+console.log("server started on port " + port)
