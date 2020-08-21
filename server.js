cdc4a0d9a24be6193883c99445e785ef6bfe6597
Tin Pham <phamkientin@gmail.com>
2020-08-21 09:54:03 +0700

    asdfad

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 5001
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.listen(port, () => console.log("Listening on Port", port)) 
 
-exports.app = functions.https.onRequest(app)
+// exports.app = functions.https.onRequest(app)
